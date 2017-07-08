fb89c301d2f97a7aa4cec08071d8d083a37615a3
Eric Bailey <nerflad@gmail.com>
2017-07-08 01:41:51 -0400

    make new-products.sh way fancier but not much different

--- new-products.sh
+++ new-products.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-curl memphisdrumshop.com/new-products > new-products.html
-vim -c "source preprocess.vim" -c "wq" new-products.html
+fetchPage() {
+    curl -s -S -A "Mozilla/5.0 (X11; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0" \
+        memphisdrumshop.com/new-products > new-products.html;
+    vim -E -c "source preprocess.vim" -c "wq" new-products.html > /dev/null;
+}
+
+if [ -e new-products.html ]; then
+   mv new-products.html new-products.html.old;
+   fetchPage;
+   diff new-products.html new-products.html.old && nothingNew=1
+   rm new-products.html.old
+else
+    fetchPage;
+fi
+
+if [ -n '$nothingNew' ]; then
+    echo No new products.
+fi
